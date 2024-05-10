28871fc571c743344352ac2a92b327a01f4021fa
AdityaCoder6 <rajthakuraditya6@gmail.com>
2024-05-10 13:10:44 +0530

    First Commit

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import authorizeUser from "../middleware/AuthMiddleware.js";
+import { upload } from "../utils/multer.js";
+import {
+  registerUser,
+  authenticateUser,
+  fetchUsers,
+  updateUserName,
+  updateUserPassword,
+  updateUserProfilePic,
+  deleteUserProfilePic,   
+  deleteNotifications,
+  // sendOTP
+} from "../controllers/UserController.js";
+
+const router = Router();
+
+/*   Base route: /api/user   */
+router.post("/register", upload.single("profilePic"), registerUser);
+router.post("/login", authenticateUser);
+// router.post('/request-otp',sendOTP)
+router.get("/", authorizeUser, fetchUsers);
+router.put("/update/name", authorizeUser, updateUserName);
+router.put("/update/password", authorizeUser, updateUserPassword);
+router.put(
+  "/update/profile-pic",
+  authorizeUser,
+  upload.single("profilePic"),
+  updateUserProfilePic
+);
+router.put("/delete/profile-pic", authorizeUser, deleteUserProfilePic);
+router.put("/delete/notifications", authorizeUser, deleteNotifications);
+
+export default router;
